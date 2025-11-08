8592a3143f78c2be26a5e2ed6cb04bbfd1bb50c8
jornvdw80 <75698084+jornvdw80@users.noreply.github.com>
2025-11-08 16:21:29 +0100

    Add files via upload

--- render.js
+++ render.js
@@ -2,14 +2,30 @@ const dataTemplate = document.querySelector('.data-template');
 const container = document.querySelector('.container');
 console.log(dataTemplate);
 
-
 fetch('Data/users.json')
-    .then(res => res.json())
+    .then(response => response.json())
     .then(data => {
         data.map(user => {
             const card = dataTemplate.content.cloneNode(true).children[0]
             const name = card.querySelector('.name')
             const email = card.querySelector('.email')
+            name.textContent = user.firstname
+            email.textContent = user.email
+            container.append(card)
+        })
+    })
+    .catch(error => console.error(error))
+
+const news = document.querySelector('.data-news');
+const container = document.querySelector('.container');
+
+fetch('Data/users.json')
+    .then(response => response.json())
+    .then(data => {
+        data.map(user => {
+            const card = news.content.cloneNode(true).children[0]
+            const name = card.querySelector('.newsTitle')
+            const email = card.querySelector('.newsSubTitle')
             name.textContent = user.firstname
             email.textContent = user.email
             container.append(card)
