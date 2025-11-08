fc2624c304bfa21dc522c886a2bd333d6e8e82d4
jornvdw80 <75698084+jornvdw80@users.noreply.github.com>
2025-11-08 16:25:32 +0100

    Add files via upload

--- render.js
+++ render.js
@@ -1,20 +1,20 @@
-const dataTemplate = document.querySelector('.data-template');
-const container = document.querySelector('.container');
-console.log(dataTemplate);
+////const dataTemplate = document.querySelector('.data-template');
+////const container = document.querySelector('.container');
+////console.log(dataTemplate);
 
-fetch('Data/users.json')
-    .then(response => response.json())
-    .then(data => {
-        data.map(user => {
-            const card = dataTemplate.content.cloneNode(true).children[0]
-            const name = card.querySelector('.name')
-            const email = card.querySelector('.email')
-            name.textContent = user.firstname
-            email.textContent = user.email
-            container.append(card)
-        })
-    })
-    .catch(error => console.error(error))
+////fetch('Data/users.json')
+////    .then(response => response.json())
+////    .then(data => {
+////        data.map(user => {
+////            const card = dataTemplate.content.cloneNode(true).children[0]
+////            const name = card.querySelector('.name')
+////            const email = card.querySelector('.email')
+////            name.textContent = user.firstname
+////            email.textContent = user.email
+////            container.append(card)
+////        })
+////    })
+////    .catch(error => console.error(error))
 
 const news = document.querySelector('.data-news');
 const container = document.querySelector('.container');
